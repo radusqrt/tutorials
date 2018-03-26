efc97954d1dc890e45d61347d532b1909079a1c3
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2018-03-26 23:55:39 +0300

    Create KEYSPACE with Simple Strategy

---
+++ cassandra/java-projects/test1/src/CreateKeySpace.java
@@ -0,0 +1,28 @@
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+
+public class CreateKeySpace {
+
+    public static void main(String args[]){
+
+        //Query
+        String query = "CREATE KEYSPACE tp WITH replication "
+                + "= {'class':'SimpleStrategy', 'replication_factor':1};";
+
+        //creating Cluster object
+        Cluster cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
+
+        //Creating Session object
+        Session session = cluster.connect();
+
+        //Executing the query
+        session.execute(query);
+
+        //using the KeySpace
+        session.execute("USE tp");
+        System.out.println("Keyspace created");
+
+        session.close();
+        cluster.close();
+    }
+}
