998e7ac96330d5ec09d2e4cb056a624f3d9e6964
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2020-04-17 15:55:15 +0300

    Updates

---
+++ precache-manifest.0c422b52ed804fce901f43868250edd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a696557650a4ee860e02e9dafb1e565",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6ca10b1cdfbcda44df3c",
+    "url": "/monsters-rolodex/static/css/main.48540cc8.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca10b1cdfbcda44df3c",
+    "url": "/monsters-rolodex/static/js/main.5b52ad66.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
